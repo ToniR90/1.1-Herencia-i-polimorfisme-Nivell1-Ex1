d1d0ebcae94afd75bbced7908f3d1f691bcf78a0
ToniR90 <romy90.t@gmail.com>
2025-03-13 10:23:41 +0100

    Optimized main code

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,11 @@ public class Main {
         StringedInstrument bass = new StringedInstrument("Fender Bass" , 1500);
         System.out.println(bass.play());
         System.out.println(bass.toString());
+
         WindInstrument flute = new WindInstrument("Yamaha" , 20);
         System.out.println(flute.play());
         System.out.println(flute.toString());
+
         PercussionInstrument drums = new PercussionInstrument("Beringer" , 3000);
         System.out.println(drums.play());
         System.out.println(drums.toString());
